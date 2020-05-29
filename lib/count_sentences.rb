c47b255f174089af69089dc542de60868c5a815e
Brian Craycraft <craycraftb24@hotmail.com>
2020-05-29 15:43:28 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -13,10 +13,22 @@ class String
   end
 
   def question?
+    
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
 
   end
 
   def exclamation?
+    
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
 
   end
 
